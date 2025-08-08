ad214856e9d2501e2cecc344e79cd12c59b2123e
Akshara <sweetakshara.0715@gmail.com>
2025-08-07 20:04:52 -0700

    Initial Commit

---
+++ SparkRank123/SparkRank123App.swift
@@ -0,0 +1,17 @@
+//
+//  SparkRank123App.swift
+//  SparkRank123
+//
+//  Created by Tiger on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct SparkRank123App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
